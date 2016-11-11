198c069c41a2b3d846bbb26a1c535ab9a2404e63
Anoop M D <anoop.md@people10.com>
2016-11-11 06:13:49 +0530

    Support for filtering by type

--- server/api/product.js
+++ server/api/product.js
@@ -1,13 +1,32 @@
 "use strict";
 
-var Product = require('../models/product');
+var Product = require('../models/product'),
+    winston = require('winston');
 
 // Gets a list of Products
 exports.findAll = function(req, res) {
-  Product.find({}, function(err, products) {
-    if (err) {
-      return res.status(404).send('No products were found');
-    }
-    return res.json(products);
-  });
+
+  var type = req.query.type;
+
+  if(['Film', 'Audio Album'].indexOf(type) > -1){
+
+    winston.info('Fetching all products by type : ' + type);
+
+    Product.find({type : type}, function(err, products) {
+      if (err) {
+        return res.status(404).send('No products were found');
+      }
+      return res.json(products);
+    });
+  } else {
+
+    winston.info('Fetching all products');
+
+    Product.find({}, function(err, products) {
+      if (err) {
+        return res.status(404).send('No products were found');
+      }
+      return res.json(products);
+    });
+  }
 };
